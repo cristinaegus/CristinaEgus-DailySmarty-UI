3d1a2cfa8420068668337a5939da88500ff05e02
cristinaegus <egusquizacristina@gmail.com>
2024-08-13 18:09:59 +0200

    connected recentposts to redux and set up redux-thunk for api requests

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import thunk from 'redux-thunk';
+
 import reducers from './reducers';
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
 
@@ -16,11 +21,12 @@ function main() {
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-            <Route path='/'exact component={Home}/>
+            <Route path='/' exact component={Home}/>
             <Route path='/results' component={Results}/>
         </Switch>
       </BrowserRouter>
     </Provider>
     , document.querySelector('.app-wrapper'));
 }
+
 document.addEventListener('DOMContentLoaded', main);
