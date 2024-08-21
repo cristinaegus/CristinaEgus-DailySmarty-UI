f3484e0d2ec7a8c043038c5230095c875ed810d4
cristinaegus <egusquizacristina@gmail.com>
2024-08-21 11:27:06 +0200

    added SET_RESULTS_POSTS reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,17 @@
-import { SET_RECENT_POSTS } from './types';
+import { 
+
+    SET_RECENT_POSTS,
+
+    SET_RESULTS_POSTS
+
+} from './types';
+
+
+
 
 import axios from 'axios';
+
+
 
 export function fetchRecentPosts() {
     return function(dispatch) {
@@ -10,20 +21,12 @@ export function fetchRecentPosts() {
                 dispatch({
                     type: SET_RECENT_POSTS,
                     payload: response.data.posts
-
                 })
-
             })
-
     }
-
 }
 
-
-
-
 export function fetchPostsWithQuery(query) {
-
     return function(dispatch) {
 
         axios.get(`https://api.dailysmarty.com/search?q=${query}`)
@@ -32,13 +35,13 @@ export function fetchPostsWithQuery(query) {
 
                 console.log(response.data.posts);
 
-                // dispatch({
+                dispatch({
 
-                //     type: SET_RECENT_POSTS,
+                    type: SET_RESULTS_POSTS,
 
-                //     payload: response.data.posts
+                    payload: response.data.posts
 
-                // })
+                })
 
             })
 
--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,10 +1,13 @@
 import {
-    SET_RECENT_POSTS
+    SET_RECENT_POSTS,
+    SET_RESULTS_POSTS
 } from '../actions/types';
+
 const INIT_STATE = {
-    posts: [],
+    resultsPosts: [],
     recentPosts: []
 }
+
 export default function(state = INIT_STATE, action) {
     switch (action.type) {
         case SET_RECENT_POSTS:
@@ -12,6 +15,12 @@ export default function(state = INIT_STATE, action) {
             return {
                     ...state,
                     recentPosts
+                }
+        case SET_RESULTS_POSTS:
+                const resultsPosts = action.payload;
+                return {
+                    ...state, 
+                    resultsPosts
                 }
         default:
             return state;
