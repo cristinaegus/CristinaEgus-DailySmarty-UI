94fbfa68cc926c654f8bdcfa1b7f5ac5eb106a6d
cristinaegus <egusquizacristina@gmail.com>
2024-08-22 10:58:33 +0200

    began styling the home component and talked about CSS Grid in codepen

--- src/components/home.js
+++ src/components/home.js
@@ -5,22 +5,18 @@ import RecentPosts from './recentPosts';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 class Home extends Component {
-
   handleSearchBarSubmit(query) {
       this.props.fetchPostsWithQuery(query, () => {
         this.props.history.push('/results');
       });
-
   }
 
   render() {
     return (
-      <div>
-        <div>
-          <Logo/>
-          <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
-          <RecentPosts/>
-        </div>
+      <div className="home">
+        <Logo/>
+        <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+        <RecentPosts/>
       </div>
     );
   }
--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import * as actions from '../actions';
-import Post from './post';
+import {Post} from './post';
 class RecentPosts extends Component {
     componentDidMount() {
         this.props.fetchRecentPosts();
